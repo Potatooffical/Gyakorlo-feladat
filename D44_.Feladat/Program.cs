8aa6b88c1d2d64f76d3226238c8ec5c57ac9a910
Niffty Its_me_ <danorb12@gmail.com>
2025-03-26 17:35:10 +0100

    44.feladat megoldása

---
+++ D44_.Feladat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D44_.Feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//          Generáljon ötös lottószám tippeket!Biztosítsa, hogy nem lehet 2 azonos szám a kihúzottak között!
+
+            List<int> szamok=new List<int>();
+            szamok = Gamblingfeltolt(5);
+            Listakiir(szamok);
+            Console.ReadLine();
+        }
+
+        private static void Listakiir(List<int> lista)
+        {
+            Console.Write("A lista kisorsolt elemeket:"+ string.Join(" ",lista));
+        }
+
+        private static List<int> Gamblingfeltolt(int v)
+        {
+            List<int> lista = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < v; i++)
+            {
+                lista.Add(rnd.Next(1,50));
+            }
+            return lista;
+        }
+    }
+}
---
+++ D44_.Feladat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("D44_.Feladat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("D44_.Feladat")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("421a30e4-4320-4cf5-b33b-5b113a78060f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
